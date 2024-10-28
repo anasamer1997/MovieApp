2e59c20060f9477ad4ae908c34b56591761f6188
anasmatn <164742706+anasmatn@users.noreply.github.com>
2024-10-28 20:17:24 +0300

    Initial Commit

---
+++ Movie_App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie_App
+//
+//  Created by anas amer on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
